6212a68d9828007f2c54b3323fedac977337b29a
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-25 18:49:21 -0300

    Adding Styled-Components

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+const Dashboard: React.FC = () => {
+	return <Title> Explorer repositórios no Github </Title>;
+};
+
+export default Dashboard;
