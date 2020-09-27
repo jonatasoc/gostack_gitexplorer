781dbf9f4a69d2235b2ff893d9b12136749b9f8b
Jonatas de Oliveira Coelho <jonatas.oliveiracoelho@gmail.com>
2020-09-26 23:16:24 -0300

    Added Styled-Components library. Added Title and Form styles

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-	return <Title> Explorer repositórios no Github </Title>;
+	return (
+    <>
+      <img src={logoImg} alt='Github Exporer' />
+      <Title> Explorer repositórios no Github </Title>
+
+      <Form>
+        <input placeholder='Digite o nome do repoositório' />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="#">
+
+        </a>
+      </Repositories>
+    </>
+  )
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,49 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
   font-size: 48px;
   color: #3A3A3A;
+  max-width: 450px;
+
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border-radius: 5px 0 0 5px;
+    border: 0;
+    color: #3A3A3A;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    background: #04D361;
+    border-radius: 0px 5px 5px 0px;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color: 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04D361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+
 `;
